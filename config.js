b503a7277d0c1c17001463db61400988a23c32e5
Rasyid Abdillah <47531796+RasyidAbdillah1406@users.noreply.github.com>
2020-01-30 14:30:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    consumer_key: 'Ypiojp0XrcG31vXVjo0aEdSdH',
-    consumer_secret: 'mr4agqSzojVQLExI72s7NK6B4UvSozcItKzoQWJXwdbUq1jRCo',
-    access_token: '1150093757393461248-jKAwFSL9yWL5fEqjNDLIK6twjRqV3v',
-    access_token_secret: 'gABHO7aL8svg6wkCHV93bVnA3QUjhHw7CEl9MYRAeKGlK'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
 }
